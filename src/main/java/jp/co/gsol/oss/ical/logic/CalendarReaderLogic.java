789ae7a802f1e841f474a9f4f25658658e7077c0
masakij <masakij@users.noreply.github.com>
2016-07-28 15:40:07 +0900

    [FIX] ユーザ情報が取得できない時は、AccountContextのlocaleを使う (例外を投げなくする)

--- src/main/java/jp/co/gsol/oss/ical/logic/CalendarReaderLogic.java
+++ src/main/java/jp/co/gsol/oss/ical/logic/CalendarReaderLogic.java
@@ -1,5 +1,7 @@
 package jp.co.gsol.oss.ical.logic;
 
+import java.util.Optional;
+
 import org.openide.util.MapFormat;
 import org.seasar.extension.jdbc.JdbcManager;
 
@@ -8,6 +10,8 @@ import com.google.common.collect.ImmutableMap;
 import jp.co.gsol.oss.ical.config.general.GsolIcalConfigCont;
 import jp.co.gsol.oss.ical.exception.ICalException;
 import jp.co.gsol.oss.ical.model.ICalendar;
+import jp.co.intra_mart.foundation.context.Contexts;
+import jp.co.intra_mart.foundation.context.model.AccountContext;
 import jp.co.intra_mart.foundation.exception.BizApiException;
 import jp.co.intra_mart.foundation.i18n.datetime.DateTime;
 import jp.co.intra_mart.foundation.master.user.UserManager;
@@ -44,7 +48,6 @@ public class CalendarReaderLogic {
      * @param endDate 終了日
      * @return ユーザのカレンダーオブジェクト
      * @throws ICalException {@link IacScheduleReaderLogic#findAllByUserCd(String, DateTime, DateTime, java.util.Locale)}
-     * または、 {@link #getUser(String, DateTime)}
      */
     public final ICalendar read(
             final String userCd, final DateTime refDate,
@@ -57,30 +60,28 @@ public class CalendarReaderLogic {
                     refDate.getTimeZone(),
                     iacScheduleReaderLogic.findAllByUserCd(userCd,
                             startDate, endDate,
-                            getUser(userCd, refDate).getDefaultLocale()));
+                            getUser(userCd, refDate)
+                                .map(u -> u.getDefaultLocale())
+                                .orElse(Contexts.get(AccountContext.class).getLocale())
+                            ));
         } catch (final BizApiException e) {
             throw new ICalException(e);
         }
     }
     /**
-     * userCd -> User.
+     * userCd -> Optional<User>.
      * @param userCd user code
      * @param date 検索基準日
      * @return ユーザオブジェクト
      * @throws BizApiException
      * {@link jp.co.intra_mart.foundation.master.user.UserManager#getUser(jp.co.intra_mart.foundation.master.user.model.IUserBizKey, java.util.Date)}
-     * @throws ICalException ユーザの情報が取得できないとき
      */
-    private User getUser(final String userCd, final DateTime date)
-            throws BizApiException, ICalException {
+    private Optional<User> getUser(final String userCd, final DateTime date)
+            throws BizApiException {
         final UserManager um = new UserManager();
         final UserBizKey key = new UserBizKey();
         key.setUserCd(userCd);
         final User user = um.getUser(key, date.getDate());
-        if (user == null)
-            throw new ICalException(
-                    "User's info is missing. "
-                    + "userCd: " + userCd + " date: " + date);
-        return user;
+        return Optional.ofNullable(user);
     }
 }
